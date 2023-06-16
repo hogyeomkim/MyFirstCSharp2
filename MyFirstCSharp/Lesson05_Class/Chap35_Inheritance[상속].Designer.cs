c7700433f9605754a01155356e61a0d613ed1f13
hogyeomkim <126076860+hogyeomkim@users.noreply.github.com>
2023-06-16 15:36:13 +0900

    마지막

--- MyFirstCSharp/Lesson05_Class/Chap35_Inheritance[상속].Designer.cs
+++ MyFirstCSharp/Lesson05_Class/Chap35_Inheritance[상속].Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace MyFirstCSharp.Lesson05_Class
+﻿namespace MyFirstCSharp
 {
     partial class Chap35_Inheritance
     {
---
+++ MyFirstCSharp/Lesson06_FinalExam/문제/Final_Exam02_3_UserReg[사용자 등록].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstCSharp
+{
+    public partial class Final_Exam02_3_UserReg : Form
+    {
+        public Final_Exam02_3_UserReg()
+        {
+            InitializeComponent();
+        }
+    }
+}
