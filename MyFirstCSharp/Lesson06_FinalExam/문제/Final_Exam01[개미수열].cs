68527179e70ea45b65ec90877f49da7f9a1c50f2
hogyeomkim <126076860+hogyeomkim@users.noreply.github.com>
2023-06-17 00:09:48 +0900

    주석 개쩌누

--- MyFirstCSharp/Lesson06_FinalExam/문제/Final_Exam01[개미수열].cs
+++ MyFirstCSharp/Lesson06_FinalExam/문제/Final_Exam01[개미수열].cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MyFirstCSharp
@@ -19,44 +13,52 @@ namespace MyFirstCSharp
 
         private void btnResult_Click(object sender, EventArgs e)
         {
-            int n = 15; // 출력할 개미수열의 번째
+            int n = 15;                                                 // 출력할 15 번째 개미수열
 
-            string antSequence = GetAntSequence(n);
-            MessageBox.Show($"개미수열 {n}번째: {antSequence}");
+            string antSequence = GenerateAntSequence(n);                // 개미수열을 생성하는 메서드를 호출하여
+                                                                        // n번째 개미수열을 생성하고 결과를 antSequence 변수에 저장
+
+            MessageBox.Show($"개미수열 {n}번째: {antSequence}");        // 메시지 박스로 개미수열을 출력
         }
 
-        private string GetAntSequence(int n)
+        /// <summary>
+        /// 주어진 번째의 개미수열을 생성하는 메서드입니다.
+        /// </summary>
+        /// <param name="n">개미수열의 번째</param>
+        /// <returns>개미수열 문자열</returns>
+        /// 
+
+        private string GenerateAntSequence(int n)
         {
-            if (n == 1)
+            StringBuilder sequence = new StringBuilder("1");                    // 초기 개미수열을 "1"로 설정
+
+            // n번째 개미수열을 생성하기 위한 반복문
+            for (int i = 1; i < n; i++)
             {
-                return "1";
+                StringBuilder nextSequence = new StringBuilder();
+                int count = 1;
+                char currentChar = sequence[0];                                 // 현재 숫자기록하는 변수 추가
+
+                // 현재 개미수열을 순회하며 다음 개미수열을 생성
+                for (int j = 1; j < sequence.Length; j++)
+                {
+                    if (sequence[j] == currentChar)                             // 현재 숫자와 이전 숫자가 동일하다면
+                    {
+                        count++;                                                // 연속된 숫자의 개수(count)를 증가
+                    }
+                    else
+                    {
+                        nextSequence.Append(currentChar).Append(count);         // 현재 숫자와 연속된 숫자의 개수를 StringBuilder에 추가
+                        count = 1;                                              // 새로운 연속된 숫자의 개수를 1로 초기화
+                        currentChar = sequence[j];                              // 현재 숫자를 갱신
+                    }
+                }
+
+                nextSequence.Append(currentChar).Append(count);                 // 마지막 숫자와 연속된 숫자의 개수를 StringBuilder에 추가
+                sequence = nextSequence;                                        // 다음 번째 개미수열을 현재 개미수열로 설정
             }
 
-            string previousSequence = GetAntSequence(n - 1);
-            string currentSequence = "";
-            int count = 1;
-
-            for (int i = 1; i < previousSequence.Length; i++)
-            {
-                if (previousSequence[i] == previousSequence[i - 1])
-                {
-                    count++;
-                }
-                else
-                {
-                    currentSequence += count.ToString() + previousSequence[i - 1];
-                    count = 1;
-                }
-            }
-
-            currentSequence += count.ToString() + previousSequence[previousSequence.Length - 1];
-
-            return currentSequence;
+            return sequence.ToString();                                         // 최종적으로 생성된 개미수열을 문자열 형태로 반환
         }
     }
-
-
-
-
-
 }
